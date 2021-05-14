8959c982bfee1189b6d7bd4d1a75322abcc11496
Rocco Orefice <rocco.orefice92@gmail.com>
2021-05-14 12:21:50 +0200

    utilizzo di pipes esistenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {registerLocaleData} from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+import { UcfirstPipe } from './ucfirst.pipe';
+registerLocaleData(localeIt);
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UcfirstPipe
   ],
   imports: [
     BrowserModule,
